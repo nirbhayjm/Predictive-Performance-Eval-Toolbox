1613ec41f8c87fefe7e8f3f7b4d1a34c25299253
Nirbhay Modhe <nmodhe@emory.edu>
2023-05-26 12:34:17 -0400

    Added setup and init for package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name="ppe_toolbox",
+    description="Predictive-Performance-Eval-Toolbox (PPET)
+    # url="",
+    version="0.0.1",
+    # packages=find_packages(),
+    # uthor="SongyiGao",
+    # author_email="songyigao@gmail.com",
+    #python_requires=">=3.9",
+    # install_requires=[
+    #     "loguru",
+    #     "numpy",
+    #     "scipy",
+    #     "h5py",
+    #     "tqdm",
+    # ],
+)
